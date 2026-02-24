88b90eebc3f68bee59c6343fc011d0ad3d9cc238
rupnath444 <147916970+rupnath444@users.noreply.github.com>
2026-02-24 10:54:21 +0530

    update image

--- images/urls.py
+++ images/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('imagelist', views.image_list, name='imagelist'),
+    path('', views.image_list, name='imagelist'),
     path('uploadimage', views.upload_image, name='uploadimage'),
+    path('updateimage/<int:pk>/', views.update_image, name='updateimage'),
 ]
--- images/views.py
+++ images/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Image
+from django.shortcuts import get_object_or_404
 from .forms import ImageForm
 
 def image_list(request):
@@ -22,4 +23,14 @@ def upload_image(request):
 def update_image(request,pk):
     image_instance=get_object_or_404(Image,pk=pk)
     
-    if request.method==
+    if request.method== 'POST':
+        form=ImageForm(request.POST,request.FILES,instance=image_instance)
+        if form.is_valid():
+            if 'image'  in request.FILES:
+                pass
+            form.save()
+            messages.success(request,'Image updated successfully!')
+    else: 
+        form=ImageForm(instance=image_instance)
+    context = {'form': form,'image_instance':image_instance}
+    return render(request,'images/update_image.html',context)
